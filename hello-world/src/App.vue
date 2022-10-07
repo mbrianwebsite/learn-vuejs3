a03136e8d82080ceff95b0503bab2455bceb3ab9
MBrian Website <muhammad.brian.27@gmail.com>
2022-10-07 19:23:58 +0700

    BINDING TEXT

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,17 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <div>
+    <div>{{ greet }} {{ name }}</div>
+    <div v-text="channel"></div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      greet: "Hello",
+      name: "Bro",
+      channel: "MBrian Website",
+    };
+  },
+};
 </script>
 
 <style>
