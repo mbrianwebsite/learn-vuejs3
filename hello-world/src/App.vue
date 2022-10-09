e90772bb3e4db6f4843fd13612e29dfd127ab327
MBrian Website <muhammad.brian.27@gmail.com>
2022-10-09 20:40:01 +0700

    BINDING CLASSES

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -7,6 +7,21 @@
     <div v-html="channelBold"></div>
     <div v-bind:id="headingId">Heading</div>
     <button v-bind:disabled="isDisabled">Button</button>
+    <h1 v-bind:class="status">Status</h1>
+    <h1 v-bind:class="isItalic && status">Italic</h1>
+    <h1 v-bind:class="isItalic ? status : 'strong'">Bold Or Italic</h1>
+    <h1 v-bind:class="[status, 'strong']">Bold and Italic</h1>
+    <h1 v-bind:class="[isItalic && status, isItalic ? status : 'strong']">
+      Function at Array
+    </h1>
+    <h1
+      v-bind:class="{
+        toitalic: isItalic,
+        strong: !isItalic,
+      }"
+    >
+      Bold and Italic
+    </h1>
   </div>
 </template>
 
@@ -23,6 +38,8 @@ export default {
       channelBold: "<b>MBrian Website</b>",
       headingId: "heading",
       isDisabled: true,
+      status: "toitalic",
+      isItalic: true,
     };
   },
 };
@@ -36,5 +53,13 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.toitalic {
+  font-style: italic;
+}
+
+.strong {
+  font-weight: 900;
 }
 </style>
