fb1bfc20f7583856cbeff000d392ab80a87c6bf2
MBrian Website <muhammad.brian.27@gmail.com>
2022-10-10 09:17:21 +0700

    CONDITIONAL RENDERING 2

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,18 +1,16 @@
 <template>
   <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
   <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-  <div>
-    <h2 v-if="num === 0">This Number Is Zero</h2>
-    <h2 v-else-if="num < 0">This Number Is Negative</h2>
-    <h2 v-else-if="num > 0">This Number Is Positive</h2>
-    <h2 v-else>This Not A Number</h2>
+  <h2 v-if="num === 0">This Number Is Zero</h2>
+  <h2 v-else-if="num < 0">This Number Is Negative</h2>
+  <h2 v-else-if="num > 0">This Number Is Positive</h2>
+  <h2 v-else>This Not A Number</h2>
 
-    <template v-if="display == true">
-      <h2>Muhammad Brian</h2>
-      <h2>Website Developer</h2>
-      <h2>MBrian Website</h2>
-    </template>
-  </div>
+  <template v-if="display == true">
+    <h2>Muhammad Brian</h2>
+    <h2>Website Developer</h2>
+    <h2>MBrian Website</h2>
+  </template>
 </template>
 
 <script>
