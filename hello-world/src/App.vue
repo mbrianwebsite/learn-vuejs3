77410606dfc47ef159bfaf9c21708031b1af3805
MBrian Website <muhammad.brian.27@gmail.com>
2022-10-10 09:02:24 +0700

    CONDITIONAL RENDERING

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -2,31 +2,16 @@
   <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
   <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   <div>
-    <div>{{ greet }} {{ name }}</div>
-    <div v-text="channel"></div>
-    <div v-html="channelBold"></div>
-    <div v-bind:id="headingId">Heading</div>
-    <button v-bind:disabled="isDisabled">Button</button>
-    <h1 v-bind:class="status">Status</h1>
-    <h1 v-bind:class="isItalic && status">Italic</h1>
-    <h1 v-bind:class="isItalic ? status : 'strong'">Bold Or Italic</h1>
-    <h1 v-bind:class="[status, 'strong']">Bold and Italic</h1>
-    <h1 v-bind:class="[isItalic && status, isItalic ? status : 'strong']">
-      Function at Array
-    </h1>
-    <h1
-      v-bind:class="{
-        toitalic: isItalic,
-        strong: !isItalic,
-      }"
-    >
-      Bold and Italic
-    </h1>
-    <h2 v-bind:style="headerStyleObject">Object Style</h2>
-    <h2 v-bind:style="[headerStyleObject, borderStyleObject]">
-      Array Object Style
-    </h2>
-    <h2 :style="[headerStyleObject, borderStyleObject]">V-Bind Short Hand</h2>
+    <h2 v-if="num === 0">This Number Is Zero</h2>
+    <h2 v-else-if="num < 0">This Number Is Negative</h2>
+    <h2 v-else-if="num > 0">This Number Is Positive</h2>
+    <h2 v-else>This Not A Number</h2>
+
+    <template v-if="display == true">
+      <h2>Muhammad Brian</h2>
+      <h2>Website Developer</h2>
+      <h2>MBrian Website</h2>
+    </template>
   </div>
 </template>
 
@@ -37,21 +22,8 @@ export default {
   name: "App",
   data() {
     return {
-      greet: "Hello",
-      name: "Bro",
-      channel: "MBrian Website",
-      channelBold: "<b>MBrian Website</b>",
-      headingId: "heading",
-      isDisabled: true,
-      status: "toitalic",
-      isItalic: true,
-      headerStyleObject: {
-        color: "orange",
-        fontWeight: 900,
-      },
-      borderStyleObject: {
-        border: "1px solid green",
-      },
+      num: "This not a number",
+      display: true,
     };
   },
 };
@@ -65,13 +37,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-
-.toitalic {
-  font-style: italic;
-}
-
-.strong {
-  font-weight: 900;
 }
 </style>
