72b6547e9cd82e1dcfe6d26dc65a78d4b6a3292c
MBrian Website <muhammad.brian.27@gmail.com>
2022-10-26 19:31:25 +0700

    IMMEDIATE AND DEEP WATCHERS

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -5,6 +5,12 @@
     <button @click="volume -= 2">Decrease</button>
     <button @click="volume += 2">Increase</button>
   </div>
+  <input type="text" v-model="movie" /><br />
+  <input type="text" v-model="movieInfo.title" /><br />
+  <input type="text" v-model="movieInfo.actor" /><br />
+  <div>
+    <button @click="movieList.push('Wonder Man')">Add Movie</button>
+  </div>
 </template>
 
 <script>
@@ -15,6 +21,12 @@ export default {
   data() {
     return {
       volume: 0,
+      movie: "Batman",
+      movieInfo: {
+        title: "",
+        actor: "",
+      },
+      movieList: ["Batman", "Superman"],
     };
   },
   methods: {},
@@ -26,6 +38,26 @@ export default {
           "Listening to a high voulme for a long time may damage your hearing"
         );
       }
+    },
+    movie: {
+      handler(newValue) {
+        console.log(`Calling API with value = ${newValue}`);
+      },
+      immediate: true,
+    },
+    movieInfo: {
+      handler(newValue) {
+        console.log(
+          `Calling API with value Title = ${newValue.title} and Actor = ${newValue.actor}`
+        );
+      },
+      deep: true,
+    },
+    movieList: {
+      handler(newValue) {
+        console.log(`Updated Value Movie List ${newValue}`);
+      },
+      deep: true,
     },
   },
 };
