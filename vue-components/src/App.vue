800993417585c851db5332addbcd524f3bd77d83
MBrian Website <muhammad.brian.27@gmail.com>
2022-10-28 20:05:06 +0700

    COMPONENTS

--- vue-components/src/App.vue
+++ vue-components/src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <GreetWord />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GreetWord from "./components/GreetWord.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    GreetWord,
+  },
+};
 </script>
 
 <style>
---
+++ vue-components/src/components/GreetWord.vue
@@ -0,0 +1,11 @@
+<template>
+  <h2>Hello Brian</h2>
+</template>
+
+<script>
+export default {
+  name: "GreetWord",
+};
+</script>
+
+<style scoped></style>
